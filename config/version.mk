3258eeb451c64d356e88e90c172d39ed93f41c80
XXMrHyde <xxmrhyde@gmx.co.uk>
2014-10-28 17:30:03 +0100

    Changes for DarkCyanKat:
    - Remove all unneeded additons
    - Change Darkkat version to DarkCyanKat version

--- config/version.mk
+++
@@ -1,6 +0,0 @@
-# Versioning of the ROM
- ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(DK_RELEASE_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.dk.version=$(ROM_VERSION)
-
