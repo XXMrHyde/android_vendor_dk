abc5aaaf81de019a04aff890164b17955fa356a8
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-02-23 16:02:23 +0100

    Rename target package, (2/2)

--- config/version.mk
+++ config/version.mk
@@ -1,7 +1,6 @@
 # Versioning of the ROM
- ROM_VERSION := cm-12-$(shell date +%Y%m%d)-$(subst cm_,,$(TARGET_PRODUCT))
- DK_VERSION := $(PLATFORM_VERSION)-$(DK_RELEASE_TYPE)
+ DK_VERSION := cm-$(PRODUCT_VERSION_MAJOR)-$(shell date +%Y%m%d)-$(subst cm_,,$(TARGET_PRODUCT))
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.dk.version=$(ROM_VERSION) \
-    ro.dk.cm.version=12
+    ro.dk.version=$(DK_VERSION) \
+    ro.dk.cm.version=$(PRODUCT_VERSION_MAJOR)
