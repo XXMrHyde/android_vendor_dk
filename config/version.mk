9b28ea314d2f741a80e8bff11a3900b05af8fd88
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-07-19 19:16:14 +0200

    Change ROM- and DK-VERSION:
    ROM VERSION:
    <BUILD_ID>-<date>-<DK_RELEASE_TYPE>-<TARGET_PRODUCT>
    (dk-MTC19Z-20160719-BETA-flo)
    
    DK-VERSION:
    <BUILD_ID>-<date>-<DK_RELEASE_TYPE>
    (MTC19Z-BETA)

--- config/version.mk
+++ config/version.mk
@@ -1,6 +1,6 @@
 # Versioning of the ROM
- ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(DK_RELEASE_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
- DK_VERSION := $(PLATFORM_VERSION)-$(DK_RELEASE_TYPE)
+ ROM_VERSION := $(BUILD_ID)-$(shell date +%Y%m%d)-$(DK_RELEASE_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
+ DK_VERSION := $(BUILD_ID)-$(DK_RELEASE_TYPE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.dk.version=$(ROM_VERSION)
