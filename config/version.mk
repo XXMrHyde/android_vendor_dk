a799cffea745e66801298cc93a44fc4c458ba0ee
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-07-19 20:37:07 +0200

    Change ROM_VERSION, remame and change DK_VERSION and DK_RELEASE_TYPE, (2/2):
    DK_BUILD_VERSION:
    <BUILD_ID>-<date>-<TARGET_PRODUCT>
    (MTC19Z-20160719-flo)
    
    Also add ro.dk.build.type (DK_BUILD_TYPE)
    to the build.prop,
    and rename ro.dk.version to ro.dk.build.version

--- config/version.mk
+++ config/version.mk
@@ -1,7 +1,7 @@
 # Versioning of the ROM
  ROM_VERSION := $(BUILD_ID)-$(shell date +%Y%m%d)-$(DK_RELEASE_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
- DK_VERSION := $(BUILD_ID)-$(DK_RELEASE_TYPE)
+ DK_BUILD_VERSION := $(BUILD_ID)-$(shell date +%Y%m%d)-$(subst dk_,,$(TARGET_PRODUCT))
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.dk.version=$(ROM_VERSION)
-
+    ro.dk.build.version=$(DK_BUILD_VERSION) \
+    ro.dk.build.type=$(DK_BUILD_TYPE)
